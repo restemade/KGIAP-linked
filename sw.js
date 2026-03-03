bd5c690955d48633a2337f3f6b18535df0ffefdd
Dinmukhammed <dimash.opg24@gmail.com>
2026-03-03 21:43:00 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "kgi-v4";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
